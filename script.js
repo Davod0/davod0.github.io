55a4545d513f27476d993b4aa42a5c533baa47a7
Davod <davodnikoyii@gmail.com>
2025-09-06 00:52:07 +0200

    Enhance portfolio layout and content

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log("Davod Portfolio loaded!");
+
+    document.body.style.opacity = 0;
+    setTimeout(() => { document.body.style.opacity = 1; }, 100);
 });
